abe6f78a5f699aa62dfce95214cafaa01042b616
Colby Wilson <colbywilson@hotmail.com>
2015-04-08 21:01:45 -0700

    Work in progress

--- donut.js
+++ donut.js
@@ -10,15 +10,13 @@
     this.hoursOpen = (this.close - this.open)/100;
     this.hourTotal = [];
   };
-
+  var tabledata = [];
   DonutShop.prototype.donutperhour = function () {
-    console.log("donuthour");
     var numbCustomer = Math.floor(Math.random() * this.maxCus - this.minCus) + this.minCus;
     return Math.round(numbCustomer * this.average);
   };
 
   DonutShop.prototype.donutperday = function() {
-  console.log("donuthour")
   var total=0;
   for (var i=0; i < this.hoursOpen; i++) {
     var dPH = this.donutperhour();
@@ -30,39 +28,48 @@
   }
 
   DonutShop.prototype.render = function() {
-    var row = document.createElement("tr");
-    var data = document.createElement("td");
+    var row = document.createElement('tr');
+    var data = document.createElement('th');
+    var tableName = document.createElement('td');
       data.textContent = this.store;
       row.appendChild(data);
     for (var i = 0; i < this.hourTotal.length ; i++) {
-        data=document.createElement("td");
-      data.textContent = this.hourTotal[i];
-      row.appendChild(data);
+      var ourdata = document.createElement('td');
+      ourdata.textContent = this.hourTotal[i];
+      row.appendChild(ourdata);
     };
-    data = document.createElement('td');
-    data.textContent = this.donutperday();
-    row.appendChild(data);
-
-
-  document.getElementById('table-body').appendChild(row);
+    tableName.textContent = this.donutperday();
+    row.appendChild(tableName);
+    return row;
+    // var tbody = document.getElementById('table-body');
+    // tbody.appendChild(row);
 };
   var downtown = new DonutShop(8, 43, 4.50, 'Downtown');
   var capitolHill = new DonutShop(4, 37, 2.00, 'Capitol Hill');
   var slu = new DonutShop(9, 23, 6.33, 'SLU');
   var wedgewood = new DonutShop(2, 28, 1.25, 'Wedgwood');
   var ballard = new DonutShop(8, 58, 3.75, 'Ballard');
+  tabledata.push(downtown);
+  tabledata.push(capitolHill);
+  tabledata.push(slu);
+  tabledata.push(wedgewood);
+  tabledata.push(ballard);
+  var renderAllData = function() {
+    var tableinfo = document.getElementById("table");
+    tableinfo.innerHTML = "<tr id='hours'> <td>Locations</td> <td>7:00 AM</td> <td>8:00 AM</td> <td>9:00 AM</td> <td>10:00 AM</td> <td>11:00 AM</td> <td>12:00 PM</td> <td>1:00 PM</td> <td>2:00 PM</td> <td>3:00 PM</td> <td>4:00 PM</td> <td>5:00 PM</td> <td>Total</td>";
+    tabledata.forEach(function(shop) {
+      tableinfo.appendChild(shop.render());
+    });
+  };
 
   downtown.donutperday();
   capitolHill.donutperday();
   slu.donutperday();
   wedgewood.donutperday();
   ballard.donutperday();
-
-  downtown.render();
-  capitolHill.render();
-  slu.render();
-  wedgewood.render();
-  ballard.render();
+  renderAllData();
   window.DonutShop = DonutShop;
+  window.tabledata = tabledata;
+  window.renderAllData = renderAllData;
 })();
 
--- events.js
+++ events.js
@@ -1,32 +1,24 @@
 (function() {
   var DonutShop = window.DonutShop;
-  var newRow = document.getElementById('addrow');
+  var newRow = document.getElementById('table');
   var newform = document.getElementById('form');
   var clearRow = document.getElementById('clear');
-  var tabledata = [];
-
-  var renderAllData = function() {
-    console.log("renderdata");
-    newRow.innerHTML = '';
-    tabledata.foreach(function(DonutShop) {
-      newRow.appendChild(DonutShop.render());
-    });
-  };
+  var tabledata = window.tabledata;
+  var renderAllData = window.renderAllData;
 
   var handleTableFormSubmit = function(event) {
-    console.log("tableform");
     event.preventDefault();
-
-  var newDonutShop = new DonutShop(event.target.store.value, event.target.minCus.value, event.target.maxCus.value, event.target.average.value);
+    var newDonutShop = new DonutShop(event.target.store.value, Number.parseInt(event.target.minCus.value), Number.parseInt(event.target.maxCus.value), Number.parseInt(event.target.average.value));
     event.target.store.value = null;
     event.target.minCus.value = null;
     event.target.maxCus.value = null;
     event.target.average.value = null;
     tabledata.push(newDonutShop);
-  }
-  newform.addEventListener('submit', handleTableFormSubmit);
-  clearRow.addEventListener('click', function() {
     renderAllData();
-    newRow.innerHTML = '';
-  });
+  };
+  newform.addEventListener('submit', handleTableFormSubmit);
+  // clearRow.addEventListener('click', function() {
+  //   newRow.textContent = '';
+  //   renderAllData();
+  // });
 })();
