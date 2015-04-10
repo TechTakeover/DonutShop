03769afd5fdb17f934d0a13e61f82f84e63d6515
Colby Wilson <colbywilson@hotmail.com>
2015-04-10 11:33:57 -0700

    fix table input, animate heading, animate form

--- events.js
+++ events.js
@@ -1,49 +1,42 @@
 var DonutShop = window.DonutShop;
-  var newRow = document.getElementById('table');
-  var newform = document.getElementById('form');
-  var clearRow = document.getElementById('clear');
-  var tabledata = window.tabledata;
-  var renderAllData = window.renderAllData;
+var newRow = document.getElementById('table');
+var newform = document.getElementById('form');
+var clearRow = document.getElementById('clear');
+var tabledata = window.tabledata;
+var renderAllData = window.renderAllData;
 
-  var handleTableFormSubmit = function(event) {
-    event.preventDefault();
+var handleTableFormSubmit = function(event) {
+  event.preventDefault();
 
-    var min = Number.parseInt(event.target.minCus.value);
-    var max = Number.parseInt(event.target.maxCus.value);
-    if (checkValue(min, max)) {
-      console.log('checkval is true');
+  var min = Number.parseInt(event.target.minCus.value);
+  var max = Number.parseInt(event.target.maxCus.value);
+  if (checkValue(min, max)) {
+    console.log('checkval is true');
 
-      var newDonutShop = new DonutShop(
-        Number.parseInt(event.target.minCus.value),
-        Number.parseInt(event.target.maxCus.value),
-        Number.parseInt(event.target.average.value),
-        event.target.store.value
-        );
-      event.target.minCus.value = null;
-      event.target.maxCus.value = null;
-      event.target.average.value = null;
-      event.target.store.value = null;
-      tabledata.push(newDonutShop);
-      newDonutShop.donutperday();
-      renderAllData();
-    } else {
-      console.log('false');
-    }
-    var newDonutShop = new DonutShop(event.target.minCus.value, Number.parseInt(event.target.maxCus.value), Number.parseInt(event.target.average.value), Number.parseInt(event.target.store .value));
+    var newDonutShop = new DonutShop(
+      Number.parseInt(event.target.minCus.value),
+      Number.parseInt(event.target.maxCus.value),
+      Number.parseInt(event.target.average.value),
+      event.target.store.value
+      );
     event.target.minCus.value = null;
     event.target.maxCus.value = null;
     event.target.average.value = null;
     event.target.store.value = null;
     tabledata.push(newDonutShop);
+    newDonutShop.donutperday();
     renderAllData();
-  };
-
-  function checkValue(minCus, maxCus) {
-    console.log(minCus + ";" + maxCus);
-    var result = false;
-    if (minCus < maxCus) {
-        result = true;
-      }
-        return result;
+  } else {
+    alert("You have have errors, fix them.");
   }
-  newform.addEventListener('submit', handleTableFormSubmit);
+};
+
+function checkValue(minCus, maxCus) {
+  console.log(minCus + ";" + maxCus);
+  var result = false;
+  if (minCus < maxCus) {
+      result = true;
+    }
+      return result;
+}
+newform.addEventListener('submit', handleTableFormSubmit);
